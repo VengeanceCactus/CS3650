ab17c51c473a9c886a3f255a64f3a006b188f7d0
Andrew Ricci <31999886+VengeanceCactus@users.noreply.github.com>
2019-03-10 22:19:58 -0400

    Uploading all CS3650 Files

---
+++ HW05/tokens2.c
@@ -0,0 +1,58 @@
+//Written by Andrew Ricci
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int _ac, char* _av[]){
+    char array[100];
+    char *space;
+    char invert[100][100];
+    char *hold;
+    char *other; 
+
+    printf("tokens$ ");
+    while((fgets(array, sizeof array, stdin)) != NULL){
+	array[strcspn(array, "\n")] = 0;
+	space = array + strlen(array);
+	while(isspace(*--space));
+	*(space+1) = '\0';
+
+	char *tok = array, *end = array;
+	int i = 0, j = 0;
+
+	while (tok != NULL){
+	    strsep(&end, " ");
+	    hold = tok;
+	    char delims[5] = ";|&<>";	    
+	    for(int delim_count = 0; delim_count < 5; delim_count ++){
+	        while((other = strsep(&hold, cur)) != NULL){
+		    if(j > 0){
+		        strcpy(invert[i+1], cur);
+			if(&(other[0]) != " "){
+		            strcpy(invert[i+2], other);
+			}
+		        i = i + 2;
+		    }
+		    else if(j == 0 && &(other[0]) != " "){
+		        strcpy(invert[i], other);
+		    }
+		    j++;
+	        }
+	        j = 0;
+	    }
+            tok = end;
+	    i++;
+	}
+
+	i--;
+	for(int j = 0; j <= i; i--){
+	    printf("%s\n", invert[i]);
+	}
+
+	if(_ac == 1){
+	    printf("tokens$ ");
+	}
+    }
+}
